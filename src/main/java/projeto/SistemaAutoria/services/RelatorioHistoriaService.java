393979d28955466d8c31108ddd9412c6280a9d1b
Matheus-Gomes-Soares <matheusgomessoares2003@gmail.com>
2025-11-27 09:55:59 -0300

    correcao de anotacao da service

--- src/main/java/projeto/SistemaAutoria/services/RelatorioHistoriaService.java
+++ src/main/java/projeto/SistemaAutoria/services/RelatorioHistoriaService.java
@@ -1,6 +1,7 @@
 package projeto.SistemaAutoria.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import projeto.SistemaAutoria.entities.Node;
 import projeto.SistemaAutoria.entities.RelatorioHistoria;
 import projeto.SistemaAutoria.repositories.NodeRepository;
@@ -9,7 +10,9 @@ import projeto.SistemaAutoria.repositories.RelatorioHistoriaRepository;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class RelatorioHistoriaService {
+
     @Autowired
     private RelatorioHistoriaRepository repository;
 
